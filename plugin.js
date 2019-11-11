a1c1b67df94f15e2bb9253754ebc7bf01e273239
Robin Sallis <Toutouwai@users.noreply.github.com>
2019-11-12 10:07:06 +1300

    v0.0.2
    Resets title on click to avoid URL title being loaded in PW link modal after double-click.

--- plugin.js
+++ plugin.js
@@ -2,12 +2,23 @@
 * Link Hover plugin
 *
 * @author Robin Sallis: https://github.com/Toutouwai/linkhover
-* @version 0.0.1
+* @version 0.0.2
 */
 
 CKEDITOR.plugins.add('linkhover', {
 	init: function(editor) {
 		editor.on('contentDom', function() {
+			function resetTitle(e) {
+				var target = e.data.getTarget();
+				if(target.is('a')) {
+					if(old_title) {
+						target.setAttribute('title', old_title);
+					} else {
+						target.removeAttribute('title');
+					}
+				}
+			}
+
 			var editable = editor.editable();
 			var old_title = false;
 			editable.attachListener(editable, 'mouseover', function(e){
@@ -17,16 +28,9 @@ CKEDITOR.plugins.add('linkhover', {
 					target.setAttribute('title', target.getAttribute('href'));
 				}
 			} );
-			editable.attachListener(editable, 'mouseout', function(e){
-				var target = e.data.getTarget();
-				if(target.is('a')) {
-					if(old_title) {
-						target.setAttribute('title', old_title);
-					} else {
-						target.removeAttribute('title');
-					}
-				}
-			});
+
+			editable.attachListener(editable, 'mouseout', resetTitle);
+			editable.attachListener(editable, 'click', resetTitle);
 		});
 	}
 });
